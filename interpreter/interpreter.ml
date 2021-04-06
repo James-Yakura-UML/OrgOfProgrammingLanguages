56b4851e65cd6ba3d3818a2c18d72d4cf561012c
James-Yakura-UML <65560282+James-Yakura-UML@users.noreply.github.com>
2021-04-06 18:38:02 -0400

    Pass first two checkpoints
    Added test files
    Fixed compile errors for bigstep.
    Passed bigstep tests.

--- interpreter/interpreter.ml
+++ interpreter/interpreter.ml
@@ -1,117 +1,140 @@
-type exp =
-  | True
-  | False
-  | If of exp * exp * exp
-  | Num of int
-  | IsZero of exp
-  | Plus of exp * exp
-  | Mult of exp * exp
-  | Var of string
-  | Lambda of string * typ * exp
-  | Apply of exp * exp
+type typ =  
+| TBool  
+| TInt  
+| TArrow of typ * typ  
 
-type typ = 
-  | TBool 
-  | TInt 
-  | TArrow of typ * typ
+type exp = 
+| True 
+| False 
+| If of exp * exp * exp 
+| Num of int 
+| IsZero of exp 
+| Plus of exp * exp 
+| Mult of exp * exp 
+| Var of string 
+| Lambda of string * typ * exp 
+| Apply of exp * exp   
 
-  exception Eval_error
+exception Eval_error  
+exception Type_error  
+exception Substitution_error  
 
-  exception Type_error
-
-  exception Substitution_error
-
-let rec step (e : exp) =
-match e with
-  | If (e0, e1, e2) -> match multi_step e0 with
-    | True -> multi_step (e1)
-    | False -> multi_step (e2)
-    | _ -> raise Eval_error
-  | IsZero (n) -> match multi_step n with
-    | Num (n1) -> match n1 with
-      | 0 -> True
-      | _ => False
-    | _ -> raise Eval_error
-  | Plus (e0, e1) -> match multi_step e0 with
-    | Num (n0) -> match multi_step e1 with 
-      | Num (n1) -> Num (n0+n1)
-      | _ -> raise Eval_error
-    | _ -> raise Eval_error
-  | Mult (e0, e1) -> match multi_step e0 with
-    | Num (n0) -> match multi_step e1 with
-      | Num (n1) -> Num (n0*n1)
-      | _ -> (n0*n1)
-    | _ -> raise Eval_error
-  | Apply (e0, e1) -> match e0 with
-    | Lambda (var, typ, e0) -> multi_step (substitution e0 var e1)
-    | _ -> raise Eval_error
-  | _ -> raise Eval_error
-
-let rec multi_step (e: exp) =
+let rec step (e : exp) = 
 match e with 
-  | True -> True
-  | False -> False
-  | Num (n) -> Num (n)
-  | Var (name) -> Var (name)
-  | Lambda (var, typ, e0) -> Lambda (var, typ, e0)
-  | _ -> step e
+| If (e0, e1, e2) ->  (
+  match multi_step e0 with 
+  | True -> multi_step (e1) 
+  | False -> multi_step (e2) 
+  | _ -> raise Eval_error
+  ) 
+| IsZero (n) ->  
+  (match multi_step n with 
+  | Num (n1) ->  
+    (match n1 with 
+    | 0 -> True 
+    | n -> False 
+    | _ -> raise Eval_error) 
+  | _ -> raise Eval_error)
+| Plus (e0, e1) ->  
+  (match multi_step e0 with 
+  | Num (n0) ->  
+    (match multi_step e1 with  
+    | Num (n1) -> Num (n0 + n1) 
+    | _ -> raise Eval_error) 
+  | _ -> raise Eval_error) 
+| Mult (e0, e1) ->  
+  (match multi_step e0 with 
+  | Num (n0) ->  
+    (match multi_step e1 with 
+    | Num (n1) -> Num (n0*n1) 
+    | _ -> (n0*n1)) 
+  | _ -> raise Eval_error) 
+| Apply (e0, e1) ->  
+  (match e0 with 
+  | Lambda (var, typ, e0) -> multi_step (substitution e0 var e1) 
+  | _ -> raise Eval_error) 
+| _ -> raise Eval_error  
 
-let rec type_check (e: exp) =
+and multi_step (e: exp) = 
+match e with  
+| True -> True 
+| False -> False 
+| Num (n) -> Num (n) 
+| Var (name) -> Var (name) 
+| Lambda (var, typ, e0) -> Lambda (var, typ, e0) 
+| _ -> step e  
+
+and type_check (e: exp) = 
 match e with
-  | True -> TBool
-  | False -> TBool
-  | Num -> TInt
-  | If (e0, e1, e2) -> match type_check e0 with
-    | TBool -> match type_check e1 with
-      | type_check e2 -> type_check e1
-      | _ -> raise Type_error
-    | _ -> raise Type_error
-  | IsZero (e0) -> match type_check e0 with
-    | TInt -> TBool
-    | _ -> raise Type_error
-  | Plus (e0, e1) -> match type_check e0 with
-    | TInt -> match type_check e1 with
-      | TInt -> TInt
-      | _ -> raise Type_error
-    | _ -> raise Type_error
-  | Mult (e0, e1) -> match type_check e0 with
-    | TInt -> match type_check e1 with
-      | TInt -> TInt
-      | _ -> raise Type_error
-    | _ -> raise Type_error
-  | Var (name) -> raise Type_error
-  | Lambda (var, t, e0) -> TArrow (t, type_check (substitution e0 var (dummy_value t)))
-  | Apply (e0, e1) -> match type_check e0 with 
-    | TArrow (left, right) -> match left with
-      | (type_check e1) -> right
-      | _ -> raise Type_error
-    | _ -> raise Type_error
-  | _ -> raise Type_error
+| True -> TBool 
+| False -> TBool 
+| If (cond,_then,_else) -> 
+  ( match type_check cond with 
+  | TBool -> 
+    ( match type_check _then with 
+    | t -> 
+      (match type_check _else with 
+      | t -> t
+      | _ -> raise Type_error)) 
+  | _ -> raise Type_error ) 
+| Num (n) -> TInt
+| IsZero (e) -> 
+  (match type_check e with
+  | TInt -> TBool
+  | _ -> raise Type_error)
+| Plus (left,right) -> 
+  (match type_check left with
+  | TInt -> 
+    (match type_check right with
+    | TInt -> TInt
+    | _ -> raise Type_error)
+  | _ -> raise Type_error)
+| Mult (left,right) -> 
+  (match type_check left with
+  | TInt -> 
+    (match type_check right with
+    | TInt -> TInt
+    | _ -> raise Type_error)
+  | _ -> raise Type_error)
+| Var (handle) -> raise Type_error
+| Lambda (var_name, var_type, expr) -> TArrow (var_type, type_check (substitution expr var_name (dummy_value var_type)))
+| Apply (lambda, argument) -> 
+  (match typeof lambda with
+  | TArrow (lambda_left_type, lambda_right_type) -> 
+    (match typeof argument with
+    | lambda_left_type -> lambda_right_type
+    | _ -> raise Type_error)
+  | t -> t)
+| _ -> raise Type_error
 
-let rec dummy_value (t: typ)=match t with (*This is a separate method in order to handle functions that output functions.*)
-  | TBool -> True
-  | TInt -> Num (0)
-  | TArrow (left, right) -> Lambda("X", left, dummy_value right)
-  | _ -> raise Type_error
+and dummy_value (t: typ)=(*This is a separate method in order to handle functions that output functions. Dummy values are purely for type checking and should never be evaluated.*) 
+match t with  
+| TBool -> True 
+| TInt -> Num (0) 
+| TArrow (left, right) -> Lambda("I want to evaluate but the clapping keeps alerting the type checker", left, dummy_value right) 
+| _ -> raise Type_error  
 
-let rec free_variables (e: exp) = match e with
-  | Var (name) -> [name]
-  | If (e0,e1,e2) -> (free_variables e0)::(free_variables e1)::(free_variables e2)
-  | IsZero (e0) -> (free_variables e0)
-  | Plus (e0, e1) -> (free_variables e0)::(free_variables e1)
-  | Mult (e0, e1) -> (free_variables e0)::(free_variables e1)
-  | Lambda (var, t, e0) -> free_variables (substitution (e0, var, dummy_value (t)))
-  | Apply (e0, e1) -> free_variables (substitution e0 var e1)
-  | _ -> []
+and free_variables (e: exp) = 
+match e with 
+| Var (name) -> [name] 
+| If (e0,e1,e2) -> (free_variables e0)::(free_variables e1)::(free_variables e2) 
+| IsZero (e0) -> (free_variables e0) 
+| Plus (e0, e1) -> (free_variables e0)::(free_variables e1) 
+| Mult (e0, e1) -> (free_variables e0)::(free_variables e1) 
+| Lambda (var, t, e0) -> free_variables (substitution (e0, var, dummy_value (t))) 
+| Apply (e0, e1) -> free_variables (substitution e0 var e1) 
+| _ -> []  
 
-let rec substitution (e1: exp) (x: string) (e2: exp) = match e1 with
-  | Var (name) -> match name with
-    | x -> e2
-    | _ -> e1
-  | If (condition, ifTrue, ifFalse) -> If (substitution condition, substitution ifTrue, substitution ifFalse)
-  | IsZero (arg) -> IsZero (substitution arg)
-  | Plus (left, right) -> Plus (substitution left, substitution right)
-  | Mult (left, right) -> Mult (substitution left, substitution right)
-  | Lambda (var, typ, sube) -> Lambda (var, typ, substitution sube)
-  | Apply (left, right) -> Apply (substitution left, substitution right)
-  | _ -> e1
+and substitution (e1: exp) (x: string) (e2: exp) = 
+match e1 with 
+| Var (name) ->  
+  (match name with 
+  | x -> e2 
+  | _ -> e1) 
+| If (condition, ifTrue, ifFalse) -> (If (substitution condition x e2, substitution ifTrue x e2, substitution ifFalse x e2)) 
+| IsZero (arg) -> IsZero (substitution arg x e2) 
+| Plus (left, right) -> Plus (substitution left x e2, substitution right x e2) 
+| Mult (left, right) -> Mult (substitution left x e2, substitution right x e2) 
+| Lambda (var, typ, sube) -> Lambda (var, typ, substitution sube x e2) 
+| Apply (left, right) -> Apply (substitution left x e2, substitution right x e2) 
+| _ -> e1
--- interpreter_bigstep/interpreter_bigstep.ml
+++ interpreter_bigstep/interpreter_bigstep.ml
@@ -1,347 +1,55 @@
 type exp=
-  |True
-  |False
-  |If of exp * exp * exp
-  |Num of int
-  |IsZero of exp
-  |Plus of exp * exp
-  |Mult of exp * exp;;
+| True
+| False
+| If of exp * exp * exp
+| Num of int
+| IsZero of exp
+| Plus of exp * exp
+| Mult of exp * exp
 
-let rec string_of_exp expr=
+exception Eval_error
+
+let rec string_of_exp (expr:exp)=
 match expr with
-  |True -> "true"
-  |False -> "false"
-  |If (e0,e1,e2) -> "if " ^ string_of_exp e0 ^ " then " ^ string_of_exp e1 ^ " else " ^ string_of_exp e2
-  |Num (n) -> string_of_int n
-  |IsZero (e) -> "isZero " ^ string_of_exp e
-  |Plus (e0,e1) -> "(" ^ string_of_exp e0 ^ " + " ^ string_of_exp e1 ^ ")"
-  |Mult (e0,e1) -> "(" ^ string_of_exp e0 ^ " * " ^ string_of_exp e1 ^ ")";;
+  True -> "true"
+| False -> "false"
+| If (e0,e1,e2) -> "if " ^ string_of_exp e0 ^ " then " ^ string_of_exp e1 ^ " else " ^ string_of_exp e2
+| Num (n) -> string_of_int n
+| IsZero (e) -> "(isZero " ^ string_of_exp e ^ ")"
+| Plus (e0,e1) -> "(" ^ string_of_exp e0 ^ " + " ^ string_of_exp e1 ^ ")"
+| Mult (e0,e1) -> "(" ^ string_of_exp e0 ^ " * " ^ string_of_exp e1 ^ ")"
+| _ -> raise Eval_error
 
-exception Eval_Error;;
-
-let rec eval expr =
-  match expr with
-    | True -> True
-    | False -> False
-    | If (e0,e1,e2) -> match eval e0 with
-      | True -> eval e1
-      | False -> eval e2
-      | _ -> raise Eval_Error
-    | Num (n) -> Num (n)
-    | IsZero (e) -> match eval e with
-      | Num (n) -> match n with
-        | 0 -> True
-        | n -> False
-      | _ -> raise Eval_Error
-    |Plus (e0,e1) -> match eval e0 with
-      | Num (n1) -> match eval e1 with
-        | Num (n2) -> Num (n1+n2)
-        | _ -> raise Eval_Error
-      | _ -> raise Eval_Error
-    |Mult (e0,e1) -> match eval e0 with
-      | Num (n1) -> match eval e1 with
-        | Num (n2) -> Num (n1*n2)
-        | _ -> raise Eval_Error
-      | _ -> raise Eval_Error
-
-
-string_of_exp(
-  Num(3)
-);;
-(*  1) 3  *)
-
-string_of_exp(
-  True
-);;
-(*  2) true *)
-
-string_of_exp(
-  False
-);;
-(*  3) false  *)
-
-string_of_exp(
-  Plus(
-    Num(3),
-    Num(2))
-);;
-(*  4) (3 + 2)  *)
-
-string_of_exp(
-  Mult(
-    Num(3),
-    Num(2)
-  )
-);;
-(*  5) (3 * 2)  *)
-
-string_of_exp(
-  Plus(
-    Num(3),
-    Plus(
-      Num(3),
-      Mult(
-        Num(2),
-        Plus(
-          Num(3),
-          Num(2)))))
-);;
-(*  6) (3 + (3 + (2 * (3 + 2))))  *)
-
-string_of_exp(
-  If(
-    True,
-    Num(3),
-    Num(5))
-);;
-(*  7) if true then 3 else 5  *)
-
-string_of_exp(
-  If(
-    False,
-    Plus(
-      Num(3),
-      Num(2)),
-    Plus(
-      Num(5),
-      Num(1)))
-);;
-(*  8) if false then (3 + 2) else (5 + 1) *)
-
-string_of_exp(
-  If(
-    Plus(
-      False,
-      True),
-    Plus(
-      Num(3),
-      False),
-    Mult(
-      Num(3),
-      Num(1)))
-);;
-(*  9) if (false + true) then (3 + false) else (3 * 1)  *)
-
-string_of_exp(
-  If(
-    IsZero(
-      Num(1)
-    ),
-    Plus(
-      Num(3),
-      Num(2)),
-    Plus(
-      Num(5),
-      Num(1)))
-);;
-(*  10) if (isZero 1) then (3 + 2) else (5 + 1) *)
-
-string_of_exp(
-  IsZero(
-    Mult(
-      Num(3),
-      Num(5))
-  )
-);;
-(*  11) (isZero (3 * 5))  *)
-
-string_of_exp(
-  IsZero(
-    If(
-      IsZero(
-        Num(1)
-      ),
-      Plus(
-        Num(3),
-        Num(2)),
-      Plus(
-        Num(5),
-        Num(1)))
-  )
-);;
-(*  12) (isZero if (isZero 1) then (3 + 2) else (5 + 1))  *)
-
-string_of_exp(
-  Plus(
-    Num(3),
-    If(
-      IsZero(
-        Num(1)
-      ),
-      Plus(
-        Num(3),
-        Num(2)),
-      Plus(
-        Num(5),
-        Num(1))))
-);;
-(*  13) (3 + if (isZero 1) then (3 + 2) else (5 + 1)) *)
-
-string_of_exp(
-  Plus(
-    Num(3),
-    Mult(
-      If(
-        IsZero(
-          Num(1)
-        ),
-        Plus(
-          Num(3),
-          Num(2)),
-        Plus(
-          Num(5),
-          Num(1))),
-      IsZero(
-        True)))
-);;
-(*  14) (3 + (if (isZero 1) then (3 + 2) else (5 + 1) * (isZero true))) *)
-
-string_of_exp(
-  If(
-    If(
-      True,
-      True,
-      False),
-    Plus(
-      Num(3),
-      Num(2)),
-    Plus(
-      Num(5),
-      Num(1)))
-);;
-(*  15) if if true then true else false then (3 + 2) else (5 + 1) *)
-
-string_of_exp(
-  If(
-    True,
-    If(IsZero(
-      Mult(
-        Num(3),
-        Num(5))
-    ),
-    Plus(
-      Num(3),
-      Num(2)),
-    Plus(
-      Num(5),
-      Num(1))),
-    If(True,
-    Mult(
-      Num(3),
-      Num(2)),
-    Mult(
-      Num(2),
-      Plus(
-        Num(3),
-        Num(2)))))
-);;
-(*  16) if true then if (isZero (3 * 5)) then (3 + 2) else (5 + 1) else if true then (3 * 2) else (2 * (3 + 2)) *)
-
-
-eval True
-(*Output: True*)
-
-eval False
-(*Output: False*)
-
-eval (Num 0)
-(*Output: (Num 0)*)
-
-eval (IsZero (Num 0))
-(*Output: True*)
-
-eval (IsZero (Plus (Num 1, Num 1)))
-(*Output: False*)
-
-eval (IsZero (Plus (Plus (Num 2, Num (-1)), Num 1)))
-(*Output: False*)
-
-eval (Plus (Plus (Num (-1), Num 1), Plus (Num (-1), Num 1)))
-(*Output: (Num 0)*)
-
-eval (Plus (Num (-1), Plus (Mult (Num 2, Num 2), Num 1)))
-(*Output: (Num 4)*)
-
-eval (Plus (Plus (Plus (Num 2, Num (-1)), Num 1), Num (-1)))
-(*Output: (Num 1)*)
-
-eval (Plus (IsZero (Plus (Num (-1), Num 1)), Num 1))
-(*Output:  Eval_error*)
-
-eval (IsZero (If (IsZero (Num 0), True, Num 0)))
-(*Output:  Eval_error*)
-
-eval
-   (IsZero
-      (If
-         ( IsZero (Mult (Num 5, Num 0))
-         , If (False, Num 0, IsZero (Plus (Num (-1), Num 0)))
-         , Num 0 )))
-(*Output:  Eval_error*)
-
-eval (If (IsZero (Plus (Num (-1), Num 1)), Num 2, True))
-(*Output:  (Num 2)*)
-
-eval
-   (If
-      ( If (IsZero (Mult (Plus (Num 1, Num (-1)), Num 1)), False, True)
-      , Mult (Num 1, Num 2)
-      , True ))
-(*Output:  True*)
-
-eval
-   (If
-      ( If (IsZero (Mult (Num 0, Num 0)), IsZero (Num 2), Num 0)
-      , Mult (Num 2, Mult (Num 1, Num 1))
-      , Plus
-          ( Plus
-              ( Plus
-                  ( Plus (If (IsZero (Num 0), Num 1, Num 0), Num (-1))
-                  , Num 1 )
-              , Num (-1) )
-          , Num 1 ) )))
-(*Output:  (Num 1)*)
-
-eval
-   (If
-      ( True
-      , If (True, Mult (If (False, Num 0, Num 1), Num 1), Num 5)
-      , Plus (Mult (Num 4, Num 1), Num 1) ))
-(*Output:  (Num 1)*)
-
-eval
-   (If
-      ( IsZero (If (IsZero (Plus (Num (-1), Num 2)), Num 0, Num 1))
-      , If
-          ( True
-          , If (False, Mult (Num 0, Num 6), Plus (Num 0, Num 1))
-          , Num 5 )
-      , Num 5 ))
-(*Output:  (Num 5)*)
-
-eval
-   (If
-      ( IsZero (Plus (Num (-1), Plus (Num 1, Plus (Num (-1), Num 1))))
-      , IsZero True
-      , Num 1 ))
-(*Output:  Eval_error*)
-
-eval
-   (Plus
-      ( Num 1
-      , Plus
-          ( Num (-1)
-          , If
-              ( IsZero (Plus (Num 1, If (True, Num 1, Num 2)))
-              , Plus (Num 1, Num 2)
-              , Mult (Num 2, Num 2) ) ) ))
-(*Output:  (Num 4)*)
-
-eval
-   (Plus
-      ( Num (-1)
-      , If
-          ( IsZero (Plus (Num 5, Num (-4)))
-          , Mult (Num 123, Plus (Num 5, Num (-4)))
-          , IsZero (Num 0) ) ))
-(*Output:  Eval_error*)
+and eval (expr:exp) =
+match expr with
+  True -> True
+| False -> False
+| If (cond, _then, _else) ->
+ ( match eval cond with
+  | True -> eval _then
+  | False -> eval _else
+  | _ -> raise Eval_error)
+| Num (n) -> Num (n)
+| IsZero (value) -> (
+  match eval value with
+  | Num (n) ->
+    (match n with
+    | 0 -> True
+    | n -> False
+    | _ -> raise Eval_error)
+  | _ -> raise Eval_error )
+| Plus (left, right) ->
+  (match eval left with
+  | Num (left_n) ->
+    (match eval right with
+    | Num (right_n) -> Num (left_n + right_n)
+    | _ -> raise Eval_error)
+  | _ -> raise Eval_error)
+| Mult (left, right) ->
+  (match eval left with
+  | Num (left_n) ->
+    (match eval right with
+    | Num (right_n) -> Num (left_n * right_n)
+    | _ -> raise Eval_error)
+  | _ -> raise Eval_error)
+| _ -> raise Eval_error
